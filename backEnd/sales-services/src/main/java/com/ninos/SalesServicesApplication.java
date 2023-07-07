be487264f3c03446e3e60bcbdc8c75a9b1c4bf8c
NinoosMoshi <nalkaton@gmail.com>
2023-07-06 19:44:59 -0500

    upload projects

---
+++ backEnd/sales-services/src/main/java/com/ninos/SalesServicesApplication.java
@@ -0,0 +1,13 @@
+package com.ninos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesServicesApplication.class, args);
+	}
+
+}
