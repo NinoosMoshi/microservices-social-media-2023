8e83d472a788d9552dedf14285671ce269c42a85
NinoosMoshi <nalkaton@gmail.com>
2023-07-08 19:00:08 -0500

    refactor xml into sql

---
+++ backEnd/user-management-gateway/src/main/resources/db/changelog/7_8_2023_6_16_alter_table_student.sql
@@ -0,0 +1,3 @@
+ALTER TABLE student DROP COLUMN name;
+ALTER TABLE student ADD age varchar(255);
+
---
+++ backEnd/user-management-gateway/src/main/resources/db/changelog/7_8_2023_9_13_create_table_student.sql
@@ -0,0 +1,5 @@
+CREATE TABLE student(
+        id bigint NOT NULL PRIMARY KEY ,
+        name varchar(255)
+        )
+
